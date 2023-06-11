7610b5d12ef38871b9393ccbe6068895e1992b1a
DanilAlexandrovich <vosmerka97@mail.ru>
2023-06-11 15:29:45 +0500

    Initial commit

--- src/task8.java
+++ src/task8.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class task8 {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class task8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = in.nextInt();
+
+        System.out.println("Введите число: ");
+        int numb = in.nextInt();
+
+        int[] arr = new int[size];
+
+        Random random = new Random();
+
+        for(int i=0;i<size;i++){
+            arr[i] = random.nextInt();
+        }
+
+        System.out.println("Массив arr: ");
+        System.out.println(Arrays.toString(arr));
+
+        int[] arrResult = filterEvenNumbers(arr, numb);
+
+        System.out.println("Массив arrResult: ");
+        System.out.println(Arrays.toString(arrResult));
+    }
+
+    public static int[] filterEvenNumbers(int[] arr, int a){
+        return Arrays.stream(arr).filter(x -> x > a).toArray();
+    }
 }
+// Напишите функцию, которая принимает на вход список целых чисел и возвращает новый список,
+// содержащий только те числа, которые больше заданного значения.
--- src/task9.java
+++ src/task9.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class task9 {
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class task9 {
+    public static void main(String[] args) {
+        String string = "Напиш1ите функцию, которая прини2мает на вход списо3к строк и возвра4щает" +
+                " новый спи5сок, содержащий толь6ко те строки, ко7торые начинаются с боль8шой буквы.";
+
+        List<String> strings = List.of(string.split(" "));
+        System.out.println("\n" + "Строка после сплитования: " + "\n");
+
+        for(String e : strings){
+            System.out.println(e);
+        }
+
+        List<String> stringsAfter = filterLengthString(strings);
+
+        System.out.println("\n" + "Строка после преобразования: " + "\n");
+        for(String e: stringsAfter){
+            System.out.println(e);
+        }
+    }
+
+    public static List<String> filterLengthString(List<String> list){
+        return list.stream()
+                .filter(s -> s.chars().allMatch(Character::isLetter))
+                .collect(Collectors.toList());
+    }
 }
+// Напишите функцию, которая принимает на вход список строк и возвращает новый список,
+// содержащий только те строки, которые содержат только буквы (без цифр и символов).
